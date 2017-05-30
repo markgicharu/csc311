fa4379d7e539e3b78d800b496c99386a615af886
markgicharu <markgicharu@hotmail.com>
2017-05-30 18:45:53 +0300

    Update method2.cs

--- method2.cs
+++ method2.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class method
+    {
+        static void Main(string[] args)
+        {
+            getNumber();
+        }
+
+        public static void getNumber()
+        {
+            Console.WriteLine("This is a void method");
+        }
+    }
+}
 
