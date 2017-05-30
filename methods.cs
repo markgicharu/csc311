29ef9d4d77f81d13a5b2b3b3cf54fa5d8f4a8298
markgicharu <markgicharu@hotmail.com>
2017-05-30 18:10:01 +0300

    Update methods.cs

--- methods.cs
+++ methods.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int a,b; //declare variables
+          Console.WriteLine("Enter the first number");
+          a=Console.ReadLine();
+          
+        }
+    }
+}
 
