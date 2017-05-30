e288466ae4eb529f238a760f60c95d1b3bd572aa
markgicharu <markgicharu@hotmail.com>
2017-05-30 19:26:25 +0300

    Create assignment.cs

---
+++ assignment.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class loop
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                Console.WriteLine("The Value of A is: " + a);
+            }
+        }
+    }
+}
+// do dowhile loop
