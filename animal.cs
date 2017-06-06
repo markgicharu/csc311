3965dcb85823a28af4acfef5594855a5e79df818
markgicharu <markgicharu@hotmail.com>
2017-06-06 19:06:51 +0300

    Create animal.cs

---
+++ animal.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP
+{
+    class Animal
+    {
+        //declare properties to be applied on objects
+        public string sex;
+        public string type;
+        public int age;
+        public string name;
+    }
+}
