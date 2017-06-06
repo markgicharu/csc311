6b29e1ebcf7ae401c3b56627a399659ce7e54f12
markgicharu <markgicharu@hotmail.com>
2017-06-06 19:56:15 +0300

    Create Animal.cs
    animal class

---
+++ Animal.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP
+{
+    class Animal
+    {
+
+
+
+        //declare properties to be applied on objects
+        public string sex;
+        public string type;
+        public int age;
+        public string name;
+       
+
+        //setter/mutator methods 
+        public void setSex(String sex)
+        {
+        this.sex = sex;
+        }
+        public void setType(String type)
+        {
+        this.type = type;
+        }
+        public void setAge(int age)
+        {
+        this.age = age;
+        }
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+
+        //getter methods
+        public string getName()
+        {
+
+            return name;
+
+        }
+        public string getSex()
+        {
+
+            return sex;
+
+        }
+        public string getType()
+        {
+
+            return type;
+
+        }
+        public int getAge()
+        {
+
+            return age;
+
+        }
+    }
+}
